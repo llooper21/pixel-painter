6bf09ff8ee6bb5d48a09a19f9deaf0d391e22c77
Junior DevLeague <jrdevleague19@Juniors-iMac.local>
2016-12-31 11:49:01 -1000

    Made into boxes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 function getBackgroundColor(idName) {
 	var element = document.getElementById(idName);
+	console.log(element);
 	var style = window.getComputedStyle(element,null).getPropertyValue("background-color");
-	document.getElementById(selectedColorName) = style;
+	document.getElementById("selectedColorName").style.backgroundColor = style;
 	//console.log(element.id)
 }
 
 function changeColorOnClick(idName) {
-	var originalElementColor = document.getElementById(selectedColorName);
-	idName = originalElementColor;
+	var originalElementColor = document.getElementById("selectedColorName").style.backgroundColor;
+	document.getElementById(idName).style.backgroundColor = originalElementColor;
+	//idName = originalElementColor;
 	//console.log(element.id)
 }
